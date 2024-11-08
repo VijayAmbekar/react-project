b89794910e1defffdbd5907b9c35e765a6826e52
VijayAmbekar <vjambekar89@gmail.com>
2024-11-08 16:38:34 +0530

    adding a Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 
 function App() {
   return (
     <div>
-      <Alert>
-        <p>Hello World!</p>
-      </Alert>
+      <Button color="danger" onClick={() => console.log("Clicked")}>
+        My Button
+      </Button>
     </div>
   );
+
+  // return (
+  //   <div>
+  //     <Alert>
+  //       <p>Hello World!</p>
+  //     </Alert>
+  //   </div>
+  // );
 
   // let items = ["Mumbai", "Delhi", "Kolkata", "London"];
 
