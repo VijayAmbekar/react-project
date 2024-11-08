8287a17d9be2048996ebd81c8a3546ef3560ca25
VijayAmbekar <vjambekar89@gmail.com>
2024-11-08 16:25:16 +0530

    passing children as argument to component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = ["Mumbai", "Delhi", "Kolkata", "London"];
-
-  const handleSelectItem = (item:string) => {
-    console.log(item);
-  }
-
   return (
     <div>
-      <ListGroup items={items} heading={"Cities"} onSelectItem={handleSelectItem}/>
+      <Alert>
+        <p>Hello World!</p>
+      </Alert>
     </div>
   );
+
+  // let items = ["Mumbai", "Delhi", "Kolkata", "London"];
+
+  // const handleSelectItem = (item:string) => {
+  //   console.log(item);
+  // }
+
+  // return (
+  //   <div>
+  //     <ListGroup items={items} heading={"Cities"} onSelectItem={handleSelectItem}/>
+  //   </div>
+  // );
 }
 
 export default App;
