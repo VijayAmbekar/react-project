33ae9cde0be450ff8a81e45c7ddd3f6766446ab4
VijayAmbekar <vjambekar89@gmail.com>
2024-11-08 14:44:30 +0530

    First commit for first react project

---
+++ src/Message.tsx
@@ -0,0 +1,9 @@
+function Message() {
+    const name='Vijay';
+    if (name)
+        return <h1>Hello {name}</h1>;
+    // JSX : Javascript XML
+    return <h1>Hello World</h1>;
+}
+
+export default Message
